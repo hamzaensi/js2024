d804593ca57de21239c9e591a5ee071023b984a3
Hamza Zagrouba <hamzaensi@gmail.com>
2024-02-19 13:39:31 +0100

    app2
    ok

---
+++ app2/app.js
@@ -0,0 +1,29 @@
+
+tab = [-2,5,9,8,-6,-1,2,-3,4,7]
+tab2= ["iset", "Tataouine", "djerba","zarzis"]
+
+function maxTab(tab) {
+
+}
+
+
+function avgTab(){
+
+}
+
+function closestToZero(){
+
+}
+
+function theLongest(tab2){
+
+}
+
+function addTask(){
+
+}
+
+
+function removeTask(){
+
+}
