0d27c5bfe8180d13da2a0e23a343ac2cf430553b
Hamza Zagrouba <hamzaensi@gmail.com>
2024-02-26 14:23:15 +0100

    taskList

---
+++ tasklist/app.js
@@ -0,0 +1,65 @@
+// Define UI Vars
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners() {
+  // DOM Load event
+ 
+  // Add task event
+  
+  // Remove task event
+  
+  // Clear task event
+  
+  // Filter tasks event
+  
+}
+
+// Get Tasks from LS
+function getTasks() {
+
+}
+
+// Add Task
+function addTask(e) {
+ 
+  
+ 
+}
+
+// Store Task
+function storeTaskInLocalStorage(task) {
+ 
+}
+
+// Remove Task
+function removeTask(e) {
+  
+}
+
+// Remove from LS
+function removeTaskFromLocalStorage(taskItem) {
+
+}
+
+// Clear Tasks
+function clearTasks() {
+ 
+}
+
+// Clear Tasks from LS
+function clearTasksFromLocalStorage() {
+  
+}
+
+// Filter Tasks
+function filterTasks(e) {
+  
+}
